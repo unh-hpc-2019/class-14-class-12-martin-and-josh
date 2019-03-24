76013cc381076309ac3f05abe0f1d9f9ab7120e0
Martin Ledoux <martin.ledoux@unh.edu>
2019-03-24 19:18:32 -0400

    Race condition - first attempt

--- linear_algebra/test_integrate.c
+++ linear_algebra/test_integrate.c
@@ -1,6 +1,9 @@
-
+#include <omp.h>
 #include <stdio.h>
 #include <math.h>
+
+
+#include "linear_algebra.h"
 
 static double
 f(double x)
@@ -11,16 +14,30 @@ f(double x)
 int
 main(int argc, char **argv)
 {
+  double t_beg = Wtime();
+
+
   const int N = 1000;
   double dx = 1. / N;
 
   double sum = 0.;
-  for (int i = 0; i < N; i++) {
-    double x0 = i * dx;
-    double x1 = (i+1) * dx;
-    sum += .5 * (f(x0) + f(x1)) * dx;
+  #pragma omp parallel
+  {
+    double local_sum=0;
+#pragma omp for
+    for (int i = 0; i < N; i++) {
+      double x0 = i * dx;
+      double x1 = (i+1) * dx;
+      sum += .5 * (f(x0) + f(x1)) * dx;
+    }
+    sum += local_sum;
+    printf("Thread %d END Time: %lf,  sum = %g\n", omp_get_thread_num(), Wtime(), sum);
   }
-  printf("sum = %g\n", sum);
+  double t_end = Wtime();
+
+  //if(sum < 0.7853) {
+    //printf("Time: %f,  sum = %g\n", t_end-t_beg, sum);
+  //}
 
   return 0;
 }
--- linear_algebra/test_openmp.c
+++ linear_algebra/test_openmp.c
@@ -11,13 +11,19 @@ main(int argc, char **argv)
   //single-threaded
   printf("Hi, just starting.\n");
   //start running multiple threads
-#pragma omp parallel
+
+  int test = -1;
+//#pragma omp parallel
+#pragma omp parallel private(test)
   {
+    //test=500;
     int thread_id = omp_get_thread_num();
     int n_threads = omp_get_num_threads();
-    printf("Hi, I'm thread %d of %d\n", thread_id, n_threads);
-
+    //printf("Hi, I'm thread %d of %d\n", thread_id, n_threads);
+    printf("Hello %d/%d test=%d\n", thread_id, n_threads, test);
+    test = thread_id;
   }
+  printf("test = %d\n", test);
 
 
   printf("almost done\n");
