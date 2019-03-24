e79bccd935d4731a78ba63bde952508ed52f4df0
Martin Ledoux <martin.ledoux@unh.edu>
2019-03-24 16:49:25 -0400

    in-class ex1, fork-join threading model

--- linear_algebra/test_openmp.c
+++ linear_algebra/test_openmp.c
@@ -8,7 +8,18 @@
 int
 main(int argc, char **argv)
 {
+  //single-threaded
+  printf("Hi, just starting.\n");
+  //start running multiple threads
 #pragma omp parallel
-  printf("Hi, I'm thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+  {
+    int thread_id = omp_get_thread_num();
+    int n_threads = omp_get_num_threads();
+    printf("Hi, I'm thread %d of %d\n", thread_id, n_threads);
+
+  }
+
+
+  printf("almost done\n");
   return 0;
 }
